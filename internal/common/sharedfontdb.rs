1708b1c29284cb5a93e8fc067e5b7b7b01752934
Alex Badics <admin@stickman.hu>
2024-08-22 17:12:23 +0200

    sharedfontdb: added set_default_font_family

--- internal/common/sharedfontdb.rs
+++ internal/common/sharedfontdb.rs
@@ -190,6 +190,11 @@ pub fn register_font_from_memory(data: &'static [u8]) -> Result<(), Box<dyn std:
     Ok(())
 }
 
+/// Set the ultimate fallback font
+pub fn set_default_font_family<S: Into<String>>(family: S) {
+    FONT_DB.with(|db| db.borrow_mut().db.set_sans_serif_family(family));
+}
+
 #[cfg(not(target_arch = "wasm32"))]
 pub fn register_font_from_path(path: &std::path::Path) -> Result<(), Box<dyn std::error::Error>> {
     let requested_path = path.canonicalize().unwrap_or_else(|_| path.to_owned());
