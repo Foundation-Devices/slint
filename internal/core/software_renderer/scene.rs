899fbbd65705219c534e90cc9f31bd639dd07e7b
Alex Badics <admin@stickman.hu>
2025-06-16 20:28:34 +0200

    software_renderer: optimize background filling

--- internal/core/software_renderer/scene.rs
+++ internal/core/software_renderer/scene.rs
@@ -231,6 +231,16 @@ impl Scene {
         self.range_valid_until_line = Length::new(validity.unwrap_or_default());
         false
     }
+
+    pub fn is_guaranteed_opaque(&self, command: &SceneCommand) -> bool {
+        match command {
+            SceneCommand::Rectangle { color } => color.alpha == 255,
+            SceneCommand::Texture { texture_index } => {
+                self.vectors.textures[*texture_index as usize].format == PixelFormat::Rgb
+            }
+            _ => false,
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
